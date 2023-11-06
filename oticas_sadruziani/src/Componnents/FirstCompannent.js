f4361f63238a47d3499c0a30852d4189884aa009
Roan Druziani <rddruziani@gmail.com>
2023-11-05 23:04:41 -0300

    Construção da interface

---
+++ oticas_sadruziani/src/Componnents/FirstCompannent.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import React from 'react';
+import img from '../Imagens/Oculos do Inicio.png'
+
+function FirstComponnentt(){
+    return (
+        <div className="Image">
+            <h1>Enchergue o mundo como ele realmente é!</h1>
+        </div>
+       
+    )
+
+}
+
+export default FirstComponnentt
